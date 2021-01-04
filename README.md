44e236d18935fc10be9a9b59046822a1a4db4f45
ironmonk88 <75920956+ironmonk88@users.noreply.github.com>
2021-01-03 17:34:37 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# hotbar-expansion
+# Monk's Hotbar Expansion
+Add-On Module for Foundry VTT
+
 This is an update to the standard Hotbar to expand and show all rows on the Hotbar on the screen at the same time.
+
+## Installation
+Simply use the install module screen within the FoundryVTT setup
+
+## Usage & Current Features
+Pressing the page number on the standard Hotbar will expand all Hotbar rows, and allow you to drag and drop on any of the rows.
+It will allow you to directly pick the row you wish to see by clicking the number beside the expanded rows, rather than paging to it.
+And clicking the trash can will allow you to clear an entire row of macros.
+
+![ExpandedHotbar](https://github.com/ironmonk88/hotbar-expansion/blob/master/screenshots/ExpandedHotbar.png?raw=true)
+
+## Known Issues
+Dragging between macros on the expanded bars isn't working.  I'm not sure why.
+
+## Bug Reporting
+Please feel free to contact me on discord if you have any questions or concerns. ironmonk88#4075
+
+## License
+This Foundry VTT module, writen by Eadorin, is licensed under [GNU GPLv3.0](https://www.gnu.org/licenses/gpl-3.0.en.html), supplemented by [Commons Clause](https://commonsclause.com/).
+
+This work is licensed under Foundry Virtual Tabletop [EULA - Limited License Agreement for module development v 0.1.6](http://foundryvtt.com/pages/license.html).
---
+++ hotbarexpansion.js
@@ -0,0 +1,78 @@
+export let debug = (...args) => {
+    if (debugEnabled > 1) console.log("DEBUG: combatdetails | ", ...args);
+};
+export let log = (...args) => console.log("monks-hotbar-expansion | ", ...args);
+export let warn = (...args) => {
+    if (debugEnabled > 0) console.warn("monks-hotbar-expansion | ", ...args);
+};
+export let error = (...args) => console.error("monks-hotbar-expansion | ", ...args);
+export let i18n = key => {
+    return game.i18n.localize(key);
+};
+
+export class MonksHotbarExpansion {
+    static tracker = false;
+    static tokenbar = null;
+    static mim = null;
+
+    static init() {
+	    log("initializing");
+        // element statics
+        
+    }
+
+    static ready() {
+    }
+
+    static async expandHotbar(e) {
+        ui.hotbar._pagecollapsed = !ui.hotbar._pagecollapsed;
+        $('#hotbar .hotbar-page')
+            .toggleClass('collapsed', ui.hotbar._pagecollapsed)
+            .toggleClass('opening', !ui.hotbar._pagecollapsed)
+            .one("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function () { $(this).removeClass('opening') });
+    }
+
+    static changePage(page, e) {
+        this.page = page;
+        this.render(true);
+        ui.hotbar._pagecollapsed = true;
+        $('#hotbar .hotbar-page').addClass('collapsed');
+    }
+
+    static async clearMacroRow(page, e) {
+        for (let i = 1; i <= 10; i++) {
+            await game.user.assignHotbarMacro(null, ((page - 1) * 10) + i);
+        }
+    }
+
+    static async renderHotbar(app, html, options) {
+        if (app._pagecollapsed == undefined)
+            app._pagecollapsed = true;
+        let hotbarpage = $('<div>').addClass('hotbar-page flexcol').toggleClass('collapsed', app._pagecollapsed);
+        for (let i = 1; i <= 5; i++) {
+            let macros = app._getMacrosByPage(i);
+
+            let macroRow = await renderTemplate(app.options.template, {
+                page: i,
+                macros: macros,
+                barClass: "",
+                dragDrop: [{ dragSelector: ".macro-icon", dropSelector: ".macro-list" }]
+            });
+            let actionBar = $('#action-bar', macroRow);
+            actionBar.removeAttr('id').addClass('action-bar');
+            $('<div>').attr('title', i18n("MONKS.HOTBAREXPANSION.clear-row")).addClass('bar-controls clear-row flexcol').append($('<i>').addClass('fas fa-trash')).on('click', $.proxy(MonksHotbarExpansion.clearMacroRow, app, i)).prependTo(actionBar);
+            actionBar.find('#macro-list').addClass('macro-list').removeAttr('id').toggleClass('selected', app.page == i);
+            actionBar.find('#hotbar-page-controls').removeAttr('id').find('.page-control').remove();
+            $('.bar-controls:not(.clear-row)', actionBar).toggleClass('selected', app.page == i).on('click', $.proxy(MonksHotbarExpansion.changePage, app, i));
+            //app.activateListeners(actionBar);
+            $(".macro", actionBar).click(app._onClickMacro.bind(app)).hover(app._onHoverMacro.bind(app));
+            hotbarpage.append(actionBar);
+        }
+        $('#macro-list', html).append(hotbarpage);
+
+        $('#hotbar-page-controls', html).on('click', $.proxy(MonksHotbarExpansion.expandHotbar));
+        log('Rendering hotbar');
+    }
+}
+
+Hooks.on('renderHotbar', MonksHotbarExpansion.renderHotbar);
